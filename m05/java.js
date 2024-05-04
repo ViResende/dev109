65b75f9c3edcb035ffb01ea5286e929e9a2e8f48
ViResende <165867539+ViResende@users.noreply.github.com>
2024-05-03 21:34:33 -0700

    Create java.js

---
+++ m05/java.js
@@ -0,0 +1,24 @@
+/* Ask user for a value */
+var userInput = prompt("Enter a number for the multiplication table:");
+
+/* Convert user input to a number */
+var table = parseInt(userInput);
+
+/* Check if the user input is valid */
+if (isNaN(table)) {
+  alert("Invalid input. Please enter a valid number.");
+} else {
+  /* Initialize the variable msg with a heading */
+  var msg = '<h2>Multiplication Table for ' + table + '</h2>';
+
+  /* Generate the multiplication table */
+  var i = 1;
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+
+  /* Write the message into the page */
+  var el = document.getElementById('blackboard');
+  el.innerHTML = msg;
+}
