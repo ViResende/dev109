72c74c5294a29177febd64c8348606dabe21889f
ViResende <165867539+ViResende@users.noreply.github.com>
2024-05-31 15:29:28 -0700

    Update script.js

--- a5/javascript/script.js
+++ a5/javascript/script.js
@@ -1,1 +1,46 @@
+var myImages = [
+    "images/background_chess.png",
+    "images/recipe.png",
+    "images/table.png",
+    "images/tech.png.png",
+    "images/woman.png"
+];
+
+var captionImages = ["game chessboard image", "recipe image woman cooking", "a math table", "Woman in tech word", "business woman"];
+var index = 0;
+
+function updateImage() {
+    document.getElementById("slideshow").src = myImages[index];
+    document.getElementById("slideshow").alt = captionImages[index];
+    document.getElementById("caption").textContent = captionImages[index];
+}
+
+function next() {
+    if (myImages.length == index + 1)
+        index = 0;
+    else
+        index++;
+    updateImage();
+}
+
+function back() {
+    if (index === 0)
+        index = myImages.length - 1;
+    else
+        index--;
+    updateImage();
+}
+
+var nextButton = document.getElementById("next");
+var previousButton = document.getElementById("previous");
+
+previousButton.addEventListener("click", back, false);
+nextButton.addEventListener("click", next, false);
+
+function autoSlide() {
+    if (document.getElementById("auto").checked)
+        next();
+}
+
+setInterval(autoSlide, 2000);
 
