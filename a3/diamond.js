a0dfe70a898735d87dea21afface64412ca348d0
ViResende <165867539+ViResende@users.noreply.github.com>
2024-06-03 22:13:37 -0700

    Update diamond.js

--- a3/diamond.js
+++ a3/diamond.js
@@ -1,33 +1,39 @@
-function createRhombus(size) {
-    let rhombus = '';
+function createRhombus() {
     
-  
-    for (let i = 0; i < size; i++) {
+    const height = parseInt(document.getElementById('rHeight').value);
+    const colorEven = document.getElementById('colorEven').value;
+    const colorOdd = document.getElementById('colorOdd').value;
+    const symbol = document.getElementById('symbol').value;
+
+    let rhombus = '';
+    const rhombusDisplay = document.getElementById('rhombusDisplay');
+    rhombusDisplay.innerHTML = '';  // Clear previous rhombus
+
+    
+    function generateLine(spaces, count) {
         let line = '';
-        for (let j = 0; j < size - i - 1; j++) {
+        for (let j = 0; j < spaces; j++) {
             line += ' ';
         }
-        for (let k = 0; k < 2 * i + 1; k++) {
-            line += '*';
+        for (let k = 0; k < count; k++) {
+            const color = k % 2 === 0 ? colorEven : colorOdd;
+            line += `<span style="color: ${color};">${symbol}</span>`;
         }
-        rhombus += line + '\n';
+        return line + '<br>';
     }
 
-   
-    for (let i = size - 2; i >= 0; i--) {
-        let line = '';
-        for (let j = 0; j < size - i - 1; j++) {
-            line += ' ';
-        }
-        for (let k = 0; k < 2 * i + 1; k++) {
-            line += '*';
-        }
-        rhombus += line + '\n';
+    
+    for (let i = 0; i < height; i++) {
+        rhombus += generateLine(height - i - 1, 2 * i + 1);
     }
 
-    return rhombus;
+    
+    for (let i = height - 2; i >= 0; i--) {
+        rhombus += generateLine(height - i - 1, 2 * i + 1);
+    }
+
+    
+    rhombusDisplay.innerHTML = rhombus;
 }
 
-// Display the rhombus
-let size = 5;
-console.log(createRhombus(size));
+
